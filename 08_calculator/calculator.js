e4c9dbda0f79c20007da33500437c538472b5c84
William E <will.elizond00@gmail.com>
2023-08-30 22:45:36 -0400

    Getting this cheddar

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
+const add = function(a,b) {
+return  a + b
+ 
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b 
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 };
 
-const multiply = function() {
+const multiply = function(array) {
+return array.reduce((accumulator, currentValue) => accumulator * currentValue, 1)
 
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0){
+    return 1;
+  } else{
+    return n * factorial(n - 1);
+  }
 };
 
 // Do not edit below this line
@@ -31,3 +38,4 @@ module.exports = {
   power,
   factorial
 };
+
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+str = str.toLowerCase();
+const cleanedStr = str.replace(/[^a-z0-9]/g, '')
+const reversedStr = cleanedStr.split('').reverse().join('');
+return cleanedStr === reversedStr;
 
 };
 
+console.log(palidromes("racecar"));
+
 // Do not edit below this line
 module.exports = palindromes;
+
